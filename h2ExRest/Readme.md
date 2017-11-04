c75c69f5a181731256a19fb654f364aeb0220cb6
anhsirksai <krishna1256@gmail.com>
2017-11-04 09:54:46 +0530

    Junit setup

--- h2ExRest/Readme.md
+++ h2ExRest/Readme.md
@@ -8,3 +8,5 @@
 7. The application is now converted to read choice from user to add / display. The current application is using mysql db backend. Properties must be updated according to your mysql db.
 
 8. The application is now converted to a web app. The restTemplate is introduced. "Display" end point works currently.
+
+9. Trying to setup Junit. 
---
+++ h2ExRest/src/test/java/com/sai/dao/CustomerRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.sai.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+class CustomerRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
