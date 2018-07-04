127ec286970d692a6f078636ab7d75cd7b98ccd9
anhsirksai <krishna1256@gmail.com>
2018-07-04 11:35:15 +0530

    picking up intellij github integration

---
+++ h2ExRest/src/main/java/code/compete/Main.java
@@ -0,0 +1,21 @@
+package code.compete;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    //Scanner s = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    br.readLine();
+    Solver.solve();
+  }
+}
+
+public class Solver{
+  public String Solve(){
+    System.out.println();
+  }
+}
